e0eeb9b89c4bbb2b6a86dcfc8b48655d1b623e65
david.days <david.c.days@gmail.com>
2016-12-26 03:51:20 -0500

    Added model transformation project, and initial database explorer classes

---
+++ src/dotnet/SystemMap/SystemMap.Models.Transform/db/DataSourceNodeBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemMap.Models.Transform.db
+{
+    public abstract class DataSourceNodeBase
+    {
+        protected string txt;
+        protected List<DataSourceNodeBase> subNodes;
+
+        protected DataSourceNodeBase(string txt, SqlConnectionStringBuilder builder)
+        {
+            this.ConnectionStringBuilder = builder;
+            this.txt = txt;
+        }
+
+        protected SqlConnectionStringBuilder ConnectionStringBuilder { get; set; }
+
+        public List<DataSourceNodeBase> Nodes
+        {
+            get
+            {
+                if (subNodes == null) subNodes = new List<DataSourceNodeBase>();
+                return subNodes;
+            }
+        }
+
+        public virtual void Load()
+        {
+            Reload();
+        }
+
+        public virtual void Reload()
+        {
+            if (subNodes == null)
+            {
+                subNodes = new List<DataSourceNodeBase>();
+            }
+            Nodes.Clear();
+            this.LoadDatabaseObjects();
+        }
+
+        protected abstract void LoadDatabaseObjects();
+
+        public override string ToString()
+        {
+            return txt;
+        }
+    }
+}
